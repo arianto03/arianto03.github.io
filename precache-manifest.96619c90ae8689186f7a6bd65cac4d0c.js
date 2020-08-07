765113bc421afef0704323bedd841224e750808c
arianto03 <aplikasiperdana@gmail.com>
2020-08-08 06:27:17 +0700

    Menyalin isi folder build

---
+++ precache-manifest.96619c90ae8689186f7a6bd65cac4d0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e777b14a4fded7c4958fe3a938418961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b644acf2ce0398db9210",
+    "url": "/static/css/main.91a5895a.chunk.css"
+  },
+  {
+    "revision": "3453b415a16c35a49f99",
+    "url": "/static/js/2.2ca56fa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ca56fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b644acf2ce0398db9210",
+    "url": "/static/js/main.c7106d52.chunk.js"
+  },
+  {
+    "revision": "28648d4b86d330626cee",
+    "url": "/static/js/runtime-main.fafa68b7.js"
+  },
+  {
+    "revision": "8ba6661d3053eb6ee76a1fa2c2da3ce5",
+    "url": "/static/media/aneka-makanan.8ba6661d.jpg"
+  }
+]);
